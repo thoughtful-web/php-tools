36b075a0a09e66775830bda9a8762f3faa352cdc
Zachary K Watkins <3458875+ZachWatkins@users.noreply.github.com>
2022-06-18 14:24:36 -0500

    Update the get and set methods to rely more on built-in error messages instead of throwing their own messages.

---
+++ src/Traits/GetSet.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * A PHP trait that provides intuitive getters and setters.
+ *
+ * @author    Zachary K. Watkins, zwatkins.it@gmail.com
+ * @copyright Zachary K. Watkins, 2022
+ * @package   ThoughtfulWeb\Tools
+ * @see       http://github.com/thoughtful-web/tools/
+ * @license   MIT
+ */
+
+namespace ThoughtfulWeb\Tools\Traits;
+
+/**
+ * Adds get and set methods to a class and handles static and object properties intuitively.
+ */
+trait GetSet {
+	/**
+	 * Store the value in this class's properties.
+	 * Can handle static and non-static properties.
+	 *
+	 * @param string $prop    The property name to store the value on.
+	 * @param array  ...$args A value to set or append, or a key to use for the next argument which is considered a value.
+	 *
+	 * @return void
+	 */
+	protected function set( string $prop, ...$args ) {
+		$arg_length = count( $args );
+		if ( 0 === $arg_length ) {
+			throw new \ArgumentCountError( 'Not enough arguments passed to ' . __CLASS__ . '->' . __FUNCTION__ . "(\"{$prop}\")." );
+		}
+		// Determine if the property is static or not.
+		$ref       = new \ReflectionProperty( __CLASS__, $prop );
+		$is_static = $ref->isStatic();
+		unset( $ref );
+		// Determine the key and value.
+		$key   = 1 === $arg_length ? null : $args[0];
+		$value = 1 === $arg_length ? $args[0] : $args[1];
+		if ( null === $key ) {
+			if ( $is_static ) {
+				self::$$prop = $value;
+			} else {
+				$this->$prop = $value;
+			}
+		} else {
+			if ( $is_static ) {
+				self::$$prop[ $key ] = $value;
+			} else {
+				$this->$prop[ $key ] = $value;
+			}
+		}
+	}
+
+	/**
+	 * Get the property value using one or more keys.
+	 * Can handle static and non-static properties.
+	 *
+	 * @param string        $prop The property name to retrieve.
+	 * @param int|string ...$keys Optional. One or more keys to retrieve from an array property.
+	 *
+	 * @return mixed
+	 */
+	protected function get( string $prop, ...$keys ) {
+		// Handle a static or non-static property.
+		$ref       = new \ReflectionProperty( __CLASS__, $prop );
+		$is_static = $ref->isStatic();
+		unset( $ref );
+		// Determine the key or keys.
+		$key_length = count( $keys );
+		if ( 0 === $key_length ) {
+			return $is_static ? self::$$prop : $this->$prop;
+		}
+		return $is_static ? array_fill_keys( $keys, self::$$prop ) : array_fill_keys( $keys, $this->$prop );
+	}
+}
---
+++ src/Traits/GetSetStatic.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * A PHP trait that provides intuitive getters and setters.
+ *
+ * @author    Zachary K. Watkins, zwatkins.it@gmail.com
+ * @copyright Zachary K. Watkins, 2022
+ * @package   ThoughtfulWeb\Tools
+ * @see       http://github.com/thoughtful-web/tools/
+ * @license   MIT
+ */
+
+namespace ThoughtfulWeb\Tools\Traits;
+
+/**
+ * Adds static get and set methods to a class, which means they can only get and set static properties.
+ */
+trait GetSetStatic {
+	/**
+	 * Store the static property value in this class's properties.
+	 *
+	 * @throws \InvalidArgumentException Ensure the class property exists and the value provided is an acceptable type.
+	 * @throws \ArgumentCountError       Ensure a key or value is passed to the method.
+	 *
+	 * @param string $prop    The property name to store the value on.
+	 * @param array  ...$args A value to set or append, or a key to use for the next argument which is considered a value.
+	 *
+	 * @return void
+	 */
+	protected static function set( string $prop, ...$args ) {
+		$arg_length = count( $args );
+		if ( 0 === $arg_length ) {
+			throw new \ArgumentCountError( 'Not enough arguments passed to ' . __CLASS__ . '::' . __FUNCTION__ . "(\"{$prop}\")." );
+		}
+		$key   = 1 === $arg_length ? null : $args[0];
+		$value = 1 === $arg_length ? $args[0] : $args[1];
+		if ( null === $key ) {
+			self::$$prop = $value;
+		} else {
+			self::$$prop[ $key ] = $value;
+		}
+	}
+
+	/**
+	 * Get the static property value.
+	 *
+	 * @param string $prop The property name to retrieve.
+	 * @param mixed  $key  Optional. A key for the property being retrieved if an array.
+	 *
+	 * @return mixed
+	 */
+	protected static function get( string $prop, ...$keys ) {
+		$key_length = count( $keys );
+		if ( 0 === $key_length ) {
+			return self::$$prop;
+		}
+		return array_fill_keys( $keys, self::$$prop );
+	}
+}
